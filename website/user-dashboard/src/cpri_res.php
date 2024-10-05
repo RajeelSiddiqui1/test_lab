47f791248b61c8645340774ba986dc7bd714049c
Rajeel Siddiqui gith <159783237+RajeelSiddiqui1@users.noreply.github.com>
2024-10-05 18:14:44 +0500

    .

---
+++ website/user-dashboard/src/cpri_res.php
@@ -0,0 +1,54 @@
+<?php
+include("conn.php");
+session_start();
+if(!isset($_SESSION["email"]) ){
+    header("login.php");
+    exit();
+}
+
+$id = $_SESSION['id'];
+?>
+
+<?php
+include("header.php");
+?>
+<div class="main-panel">
+          <div class="content-wrapper">
+           <div class="container">
+            <div class="row">
+            <table class="table table-bordered table-striped">
+            <thead class="table-dark">
+              <tr>
+                <th>Product ID</th>
+                <th>Test ID</th>
+                <th> Product Name</th>
+                
+                <th>Message</th>
+                <th>Image</th>
+                <th>Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            <?php
+              
+              $query = "SELECT *FROM `cpri_product` WHERE user_id = $id";
+              $result = mysqli_query($conn, $query);
+              while($row = mysqli_fetch_assoc($result)){ ?>
+             <tr>
+              
+                
+                <td><?php echo $row['id']?></td>
+                <td><?php echo $row['test_id']?></td>
+                <td><?php echo $row['product_name']?></td>
+                <td><?php echo substr($row['message2'],0,30).'...'?></td>
+                <td><img src="../../../images/products/<?php echo $row['product_image']?>" width="100" height="100" alt=""></td>
+              <td><?php echo $row['status2']?></td>
+              <td><a href="edit_tester_res.php?id=<?php echo $row['id']?>" class="btn" style="background-color: purple; color: white;">Edit</a></td>
+             </tr>
+             <?php }?>
+            </tbody>
+          </table>
+            </div>
+           </div>
+          </div>
--- website/user-dashboard/src/header.php
+++ website/user-dashboard/src/header.php
@@ -70,7 +70,7 @@
                   <span class="availability-status online"></span>
                 </div>
                 <div class="nav-profile-text">
-                  <p class="mb-1 text-black">David Greymaax</p>
+                  <p class="mb-1 text-black"><?php echo $_SESSION['full_name']?></p>
                 </div>
               </a>
               <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
@@ -129,7 +129,7 @@
             </li>
             
             <li class="nav-item nav-logout d-none d-lg-block">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="../../logout.php">
                 <i class="mdi mdi-power"></i>
               </a>
             </li>
@@ -153,7 +153,7 @@
                   <!--change to offline or busy as needed-->
                 </div>
                 <div class="nav-profile-text d-flex flex-column">
-                  <span class="font-weight-bold mb-2">David Grey. H</span>
+                  <span class="font-weight-bold mb-2"><?php echo $_SESSION['full_name']?></span>
                   <!-- <span class="text-secondary text-small">Project Manager</span> -->
                 </div>
                 <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
@@ -165,110 +165,21 @@
                 <i class="mdi mdi-home menu-icon"></i>
               </a>
             </li>
+
             <li class="nav-item">
-              <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <span class="menu-title">Basic UI Elements</span>
-                <i class="menu-arrow"></i>
-                <i class="mdi mdi-crosshairs-gps menu-icon"></i>
-              </a>
-              <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/ui-features/typography.html">Typography</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-                <span class="menu-title">Icons</span>
-                <i class="mdi mdi-contacts menu-icon"></i>
-              </a>
-              <div class="collapse" id="icons">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/icons/font-awesome.html">Font Awesome</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-bs-toggle="collapse" href="#forms" aria-expanded="false" aria-controls="forms">
-                <span class="menu-title">Forms</span>
-                <i class="mdi mdi-format-list-bulleted menu-icon"></i>
-              </a>
-              <div class="collapse" id="forms">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/forms/basic_elements.html">Form Elements</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
-                <span class="menu-title">Charts</span>
-                <i class="mdi mdi-chart-bar menu-icon"></i>
-              </a>
-              <div class="collapse" id="charts">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-                <span class="menu-title">Tables</span>
-                <i class="mdi mdi-table-large menu-icon"></i>
-              </a>
-              <div class="collapse" id="tables">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <span class="menu-title">User Pages</span>
-                <i class="menu-arrow"></i>
-                <i class="mdi mdi-lock menu-icon"></i>
-              </a>
-              <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/login.html"> Login </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/register.html"> Register </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/error-404.html"> 404 </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/error-500.html"> 500 </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="docs/documentation.html" target="_blank">
-                <span class="menu-title">Documentation</span>
-                <i class="mdi mdi-file-document-box menu-icon"></i>
+              <a class="nav-link" href="tester_res.php">
+                <span class="menu-title">Tester Response</span>
+                <i class="mdi mdi-home menu-icon"></i>
               </a>
             </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="cpri_res.php">
+                <span class="menu-title">CPRI Tester Response</span>
+                <i class="mdi mdi-home menu-icon"></i>
+              </a>
+            </li>
+           
           </ul>
         </nav>
         <!-- partial -->
